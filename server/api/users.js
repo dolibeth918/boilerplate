01b5a38169023b6d2c619f60030eb9b5fcf997ee
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-10-14 11:19:17 -0400

    remove unused files

--- server/api/users.js
+++ server/api/users.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res, next) => {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
+      // send everything to anyone who asks
       attributes: ['id', 'email']
     })
     res.json(users)
