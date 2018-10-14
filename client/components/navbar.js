28ab6441bf58a8a30ad332ce7715c48a22e6fa7c
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-10-14 12:05:06 -0400

    remove comments and rename

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -5,7 +5,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>BOILERPLATE</h1>
     <nav>
       {isLoggedIn ? (
         <div>
